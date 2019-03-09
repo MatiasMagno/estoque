99dd40c5ecac0006c93e67c7feac37c53880fa7f
phmiranda <pehhagah.1607@gmail.com>
2019-03-09 18:19:10 -0300

    reestruturação de diretórios dos controllers conforme o curso

---
+++ app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ProdutoController extends Controller {
+    // comentários
+    public function index() {
+        return "<h1>Listagem de Produtos</h1><hr>";
+    }
+
+    // comentários
+    public function create() {
+        //
+    }
+
+    // comentários
+    public function store(Request $request) {
+        //
+    }
+
+    // comentários
+    public function show($id) {
+        //
+    }
+
+    // comentários
+    public function edit($id) {
+        //
+    }
+
+    // comentários
+    public function update(Request $request, $id) {
+        //
+    }
+
+    // comentários
+    public function destroy($id) {
+        //
+    }
+}
