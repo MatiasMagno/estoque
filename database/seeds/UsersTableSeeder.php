94c433398529d8d7d99c9c07335698d02ec69d5f
phmiranda <pehhagah.1607@gmail.com>
2019-03-09 19:31:00 -0300

    automatização na criação da massa de dados

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -2,27 +2,22 @@
 
 use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // perfil de administrador
+class UsersTableSeeder extends Seeder {
+    // comentários
+    public function run() {
         DB::table('users')->insert([
-            'name' => 'Administrador',
-            'email' => 'admin@email.com',
-            'password' => bcrypt('123456'),
-        ]);
-
-        // perfil de usuário comum
-        DB::table('users')->insert([
-            'name' => 'Usuário Comum',
-            'email' => 'usuario@email.com',
-            'password' => bcrypt('123456'),
+            // perfil de administrador
+            [
+                'name' => 'Administrador',
+                'email' => 'admin@email.com',
+                'password' => bcrypt('123456'),
+            ],
+            // perfil de usuário comum
+            [
+                'name' => 'Usuário Comum',
+                'email' => 'usuario@email.com',
+                'password' => bcrypt('123456'),
+            ],
         ]);
     }
 }
--- database/seeds/UsuariosTableSeeder.php
+++ database/seeds/UsuariosTableSeeder.php
@@ -2,15 +2,22 @@
 
 use Illuminate\Database\Seeder;
 
-class UsuariosTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //
+class UsuariosTableSeeder extends Seeder {
+    // comentários
+    public function run() {
+        DB::table('usuarios')->insert([
+            // perfil de administrador
+            [
+                'nome' => 'Administrador',
+                'email' => 'admin@email.com',
+                'senha' => bcrypt('123456'),
+            ],
+            // perfil de usuário comum
+            [
+                'nome' => 'Usuário Comum',
+                'email' => 'usuario@email.com',
+                'senha' => bcrypt('123456'),
+            ],
+        ]);
     }
 }
